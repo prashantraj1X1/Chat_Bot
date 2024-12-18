6a7f91acbaa803a7f254cae8dea9371bc584e97f
Prashant Raj <rajprashant00011@gmail.com>
2024-12-18 11:46:10 +0530

    COmpleted chat bot part

---
+++ server.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.*;
+
+class server {
+    // constructor
+    Serversocket server;
+    Socket socket;
+
+    public server() {
+        try {
+
+        } catch (Exception e) {
+
+        }
+        server = new Serversocket();// socket with port no and storing the value in socket
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("this is to start the  server");
+        new server();// obj of server
+    }
+}
